adf8f7fbcfb5baa8b8bf2449d7e9d00a645f467e
amberry <amberry@F1092-30-2118>
2019-09-20 14:30:17 -0500

    Does It Blend?

---
+++ DoesItBlend/IBlendable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoesItBlend
+{
+    /// <summary>
+    /// Interface for items that can be blended.
+    /// </summary>
+    interface IBlendable
+    {
+        /// <summary>
+        /// Blends object
+        /// </summary>
+        /// <returns>A string ??</returns>
+        string Blend();
+
+
+    }
+}
